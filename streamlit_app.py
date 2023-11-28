8da2d4dc9e979a3845bf54b70c4bb572cf30d886
ssylin86 <150774501+ssylin86@users.noreply.github.com>
2023-11-28 16:08:02 +0800

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,14 +26,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #create the repeatable code block (called a function)
-def get_fruityvice_data(this_fruit_choice):
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+def get_fruityvice_data(this_fruit_choice): #Define function
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice) #Req API
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #Normalise data from JSON
         return fruityvice_normalized
 
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice')
-try:
+try: #test if a null error and if null then return error msg
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("Please select a friut to get information")
@@ -44,17 +44,25 @@ try:
 except URLError as e:
     streamlit.error()
 
-#troubleshooting stop running from here
-streamlit.stop()
+
 
 #import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("SELECT * from fruit_load_list")
+         return my_cur.fetchall
+
+#Add button to load the fruit
+if streamlit.button('Get Fruit Load List')
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+
+#troubleshooting stop running from here
+streamlit.stop()
 
 #Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
